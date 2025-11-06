5e539b3638c55edc0ca808ad5fa90b17f4f0a0ce
PozihameBaystar <rxrgm195@gmail.com>
2025-11-06 23:38:36 +0900

    create make_data function

--- python/make_data.py
+++ python/make_data.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-def flat_curve_data(
+def flat_curve_data1(
         L: float,
         a: float,
         t: np.ndarray,
 ) -> np.ndarray:
-    data = L*(1-np.exp(-a*t))
+    data = L * (1-np.exp(-a*t))**2
+    return data
+
+def flat_curve_data2(
+        L: float,
+        a: float,
+        t: np.ndarray,
+) -> np.ndarray:
+    data = L * np.tanh(a*t)**2
     return data
